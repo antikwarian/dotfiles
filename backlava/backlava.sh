7a2a597ad08a4e1fb633ed951b1c38ea0e16a397
Logan Talley <31964308+antikwarian@users.noreply.github.com>
2018-06-02 14:55:22 -0500

    Backup of my backup service

---
+++ backlava/backlava.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SRC="/home/logan/test/"
+SNAP="/home/logan/snaps"
+OPTS="-rltgoi --delay-updates --delete --chmod=a-w"
+MINCHANGES=20
+
+ionice -c 3 -p $$
+renice +12 -p $$
+
+rsync $OPTS $SRC $SNAP/latest >> $SNAP/rsync.log
+
+COUNT=$( wc -l $SNAP/rsync.log|cut -d" " -f1 )
+if [ $COUNT -gt $MINCHANGES ] ; then
+        DATETAG=$(date +%Y-%m-%d)
+        if [ ! -e $SNAP/$DATETAG ] ; then
+                cp -al $SNAP/latest $SNAP/$DATETAG
+                chmod u+w $SNAP/$DATETAG
+                mv $SNAP/rsync.log $SNAP/$DATETAG
+               chmod u-w $SNAP/$DATETAG
+         fi
+fi
+
